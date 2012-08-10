5e7ff08cf83185ba8a8946f3f77aeee27b1395c5
Jeremy Lakeman <Jeremy.Lakeman@gmail.com>
2012-08-10 10:41:50 +0930

    Use hash table to support multiple active calls

--- chan_vomp.c
+++ chan_vomp.c
@@ -64,7 +64,7 @@ int remote_pickup(char *cmd, int argc, char **argv, unsigned char *data, int dat
 int remote_hangup(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context);
 int remote_audio(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context);
 int remote_ringing(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context);
-int remote_keepalive(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context);
+int remote_noop(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context);
 
 static const char desc[] = "Serval Vomp Channel Driver";
 static const char type[] = "VOMP";
@@ -90,26 +90,27 @@ static const struct ast_channel_tech vomp_tech = {
 };
 
 struct vomp_channel {
-	int session_id;
-	int chan_id;
-	int channel_start;
-	int call_start;
-	int initiated;
+	int session_id; // call session id as returned by servald, used as the key for the channels collection
+	int chan_id; // unique number for generating a name for the channel
+	int channel_start; // time when we started the channel
+	int call_start; // time when we hit in-call
+	int initiated; // did asterisk start dialing?
 	struct ast_channel *owner;
 };
 
-// HACK, one active call at a time for now
-struct vomp_channel *active_call;
 struct vomp_channel *dialed_call;
 
 struct monitor_command_handler monitor_handlers[]={
-	{.command="CALLFROM",    .handler=remote_call},
-	{.command="RINGING",     .handler=remote_ringing},
-	{.command="ANSWERED",    .handler=remote_pickup},
-	{.command="CALLTO",      .handler=remote_dialing},
-	{.command="HANGUP",      .handler=remote_hangup},
-	{.command="AUDIOPACKET", .handler=remote_audio},
-	{.command="KEEPALIVE",   .handler=remote_keepalive},
+	{.command="CALLFROM",      .handler=remote_call},
+	{.command="RINGING",       .handler=remote_ringing},
+	{.command="ANSWERED",      .handler=remote_pickup},
+	{.command="CALLTO",        .handler=remote_dialing},
+	{.command="HANGUP",        .handler=remote_hangup},
+	{.command="AUDIOPACKET",   .handler=remote_audio},
+	{.command="KEEPALIVE",     .handler=remote_noop},
+	{.command="CALLSTATUS",    .handler=remote_noop},
+	{.command="MONITORSTATUS", .handler=remote_noop},
+	{.command="MONITOR",       .handler=remote_noop},
 };
 
 int chan_id=0;
@@ -137,11 +138,13 @@ static struct vomp_channel *new_vomp_channel(void){
 	vomp_state->chan_id = ast_atomic_fetchadd_int(&chan_id, +1);
 	vomp_state->channel_start = gettime_ms();
 	
-	ao2_link(channels, vomp_state);
-	
-	// HACK one active call
-	active_call = vomp_state;
 	return vomp_state;
+}
+
+static void set_session_id(struct vomp_channel *vomp_state, int session_id){
+	ast_log(LOG_WARNING, "Adding session %06x\n",session_id);
+	vomp_state->session_id = session_id;
+	ao2_link(channels, vomp_state);
 }
 
 static struct ast_channel *new_channel(struct vomp_channel *vomp_state, int state, char *context, char *ext){
@@ -170,20 +173,20 @@ static struct ast_channel *new_channel(struct vomp_channel *vomp_state, int stat
 // functions for handling incoming vomp events
 
 // find the channel struct from the servald token
+// note that ao2_find adds a reference to the returned object that must be released
 struct vomp_channel *get_channel(char *token){
-	// HACK only one active call for now
-	// TODO lookup using [channels]
-	if (!active_call)
-		return NULL;
-	int session_id=strtol(token, NULL, 16);
-	if (active_call->session_id==session_id)
-		return active_call;
-	return NULL;
+	struct vomp_channel search={
+		.session_id=strtol(token, NULL, 16),
+	};
+	struct vomp_channel *ret = ao2_find(channels, &search, OBJ_POINTER);
+	if (ret==NULL)
+		ast_log(LOG_WARNING, "Failed to find call structure for session %s (%06x)\n",token,search.session_id);
+	return ret;
 }
 
 // Send outgoing monitor messages
 
-// TODO fix servald, currently case sensitive
+// TODO fix servald, commands are currently case sensitive
 void send_hangup(int session_id){
 	monitor_client_writeline(monitor_client_fd, "hangup %06x\n",session_id);
 }
@@ -206,13 +209,16 @@ int remote_dialing(char *cmd, int argc, char **argv, unsigned char *data, int da
 	ast_log(LOG_WARNING, "remote_dialing\n");
 	if (!dialed_call)
 		return 0;
-	dialed_call->session_id=strtol(argv[0], NULL, 16);
+	// add the vomp state to our collection so we can find it later
+	set_session_id(dialed_call, strtol(argv[0], NULL, 16));
 	dialed_call=NULL;
 	return 1;
 }
 
 // CALLFROM [token] [localsid] [localdid] [remotesid] [remotedid]
 int remote_call(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context){
+	// TODO fix servald and other VOMP clients to pass extension correctly
+	// TODO add callerid...
 	char *ext = "100";//argv[2];
 	char *ctx="users";
 	ast_log(LOG_WARNING, "remote_call\n");
@@ -220,7 +226,7 @@ int remote_call(char *cmd, int argc, char **argv, unsigned char *data, int dataL
 	
 	if (ast_exists_extension(NULL, ctx, ext, 1, NULL)) {
 		struct vomp_channel *vomp_state=new_vomp_channel();
-		vomp_state->session_id=session_id;
+		set_session_id(vomp_state, session_id);
 		vomp_state->initiated=0;
 		
 		struct ast_channel *ast = new_channel(vomp_state, AST_STATE_RINGING, ctx, ext);
@@ -238,57 +244,85 @@ int remote_call(char *cmd, int argc, char **argv, unsigned char *data, int dataL
 }
 
 int remote_pickup(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context){
+	int ret=0;
 	ast_log(LOG_WARNING, "remote_pickup\n");
 	struct vomp_channel *vomp_state=get_channel(argv[0]);
-	if (!vomp_state || !vomp_state->owner) return 0;
-	
-	ast_indicate(vomp_state->owner, -1);
-	ast_queue_control(vomp_state->owner, AST_CONTROL_ANSWER);
-	return 1;
+	if (vomp_state){
+		if (vomp_state->owner){
+			// stop any audio indications on the channel
+			ast_indicate(vomp_state->owner, -1);
+			// yay, we're INCALL
+			ast_queue_control(vomp_state->owner, AST_CONTROL_ANSWER);
+			ret=1;
+		}
+		ao2_ref(vomp_state,-1);
+	}
+	return ret;
 }
 
 int remote_hangup(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context){
+	int ret=0;
 	ast_log(LOG_WARNING, "remote_hangup\n");
 	struct vomp_channel *vomp_state=get_channel(argv[0]);
-	if (!vomp_state || !vomp_state->owner) return 0;
-	ast_queue_hangup(vomp_state->owner);
-	return 1;
+	if (vomp_state){
+		if (vomp_state->owner){
+			// ask asterisk to hangup the channel
+			// that way we can let vomp_hangup do all the work to release memory
+			ast_queue_hangup(vomp_state->owner);
+			ret=1;
+		}
+		ao2_ref(vomp_state,-1);
+	}
+	return ret;
 }
 
 int remote_audio(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context){
+	int ret=0;
 	struct vomp_channel *vomp_state=get_channel(argv[0]);
-	if (!vomp_state || !vomp_state->owner) return 0;
+	if (vomp_state){
+		if (vomp_state->owner){
 	
-	// currently assumes 16bit pcm
-	struct ast_frame f = {
-		.frametype = AST_FRAME_VOICE,
-		.subclass.codec = AST_FORMAT_SLINEAR,
-		.src = "vomp_call",
-		.data.ptr = data,
-		.datalen = dataLen,
-		.samples = dataLen / sizeof(int16_t),
-	};
-	ast_queue_frame(vomp_state->owner, &f);
-	return 1;
+			// currently assumes 16bit, 8kHz pcm
+			// TODO deal with packet loss and reordering?
+			struct ast_frame f = {
+				.frametype = AST_FRAME_VOICE,
+				.subclass.codec = AST_FORMAT_SLINEAR,
+				.src = "vomp_call",
+				.data.ptr = data,
+				.datalen = dataLen,
+				.samples = dataLen / sizeof(int16_t),
+			};
+			ast_queue_frame(vomp_state->owner, &f);
+			ret=1;
+		}
+		ao2_ref(vomp_state,-1);
+	}
+	return ret;
 }
 
 // remote party has started ringing
 int remote_ringing(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context){
+	int ret=0;
 	ast_log(LOG_WARNING, "remote_ringing\n");
 	struct vomp_channel *vomp_state=get_channel(argv[0]);
-	if (!vomp_state || !vomp_state->owner) return 0;
-	ast_indicate(vomp_state->owner, AST_CONTROL_RINGING);
-	ast_queue_control(vomp_state->owner, AST_CONTROL_RINGING);
-	return 1;
+	if (vomp_state){
+		if (vomp_state->owner){
+			ast_indicate(vomp_state->owner, AST_CONTROL_RINGING);
+			ast_queue_control(vomp_state->owner, AST_CONTROL_RINGING);
+			ret=1;
+		}
+		ao2_ref(vomp_state,-1);
+	}
+	return ret;
 }
 
-int remote_keepalive(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context){
+int remote_noop(char *cmd, int argc, char **argv, unsigned char *data, int dataLen, void *context){
 	// NOOP for now, just to eliminate unnecessary log spam
 	return 1;
 }
 
-
-// create a monitor client and read incoming messages
+// thread function for the monitor client
+// reads and processes incoming messages
 static void *vomp_monitor(void *ignored){
 	struct monitor_state *state;
 	
@@ -322,8 +356,8 @@ static void *vomp_monitor(void *ignored){
 
 // create a channel for a new outgoing call
 static struct ast_channel *vomp_request(const char *type, format_t format, const struct ast_channel *requestor, const char *dest, int *cause){
-	// Note, dest could indicate some config or something, for now we don't care.
-	
+	// assume dest = servald subscriber id (sid)
+	// TODO parse dest = sid/did
 	ast_log(LOG_WARNING, "vomp_request %s/%s\n", type, dest);
 	struct vomp_channel *vomp_state=new_vomp_channel();
 	
@@ -342,9 +376,6 @@ static int vomp_hangup(struct ast_channel *ast){
 	
 	struct vomp_channel *vomp_state = (struct vomp_channel *)ast->tech_pvt;
 	
-	// HACK one active call
-	active_call = NULL;
-  
 	send_hangup(vomp_state->session_id);
 	ao2_unlink(channels, vomp_state);
 	ao2_ref(vomp_state,-1);
@@ -397,8 +428,6 @@ static int vomp_answer(struct ast_channel *ast){
 	vomp_state->call_start = gettime_ms();
 	ast_setstate(ast, AST_STATE_UP);
 	send_pickup(vomp_state);
-	// TODO local_state --> InCall
-	
 	return 0;
 }
 
@@ -423,13 +452,13 @@ static int vomp_write(struct ast_channel *ast, struct ast_frame *frame){
 
 static int vomp_hash(const void *obj, const int flags){
 	const struct vomp_channel *vomp_state = obj;
-	return vomp_state->chan_id;
+	return vomp_state->session_id;
 }
 
 static int vomp_compare(void *obj, void *arg, int flags){
 	struct vomp_channel *obj1 = obj;
 	struct vomp_channel *obj2 = arg;
-	return obj1->chan_id==obj2->chan_id ? CMP_MATCH | CMP_STOP : 0;
+	return obj1->session_id==obj2->session_id ? CMP_MATCH | CMP_STOP : 0;
 }
 
 int vomp_register_channel(void){
